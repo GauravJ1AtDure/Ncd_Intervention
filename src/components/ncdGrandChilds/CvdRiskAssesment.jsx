bd5603f5065b28a466e12f960776f2bcc5fbcf43
GauravJ1AtDure <gaurav.jadhav@duretechnologies.com>
2023-07-10 17:59:36 +0530

    working on design

--- src/components/ncdGrandChilds/CvdRiskAssesment.jsx
+++ src/components/ncdGrandChilds/CvdRiskAssesment.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from "react-redux";
+import { Parser } from 'html-to-react';
 
 function CvdRiskAssesment() {
 
@@ -24,18 +25,18 @@ function CvdRiskAssesment() {
     <button class="nav-link" id={val[10].title} data-bs-toggle="pill" data-bs-target={`#${val[10].nid}`} type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">{val[10].title}</button>
   </div>
   <div class="tab-content" id="v-pills-tabContent">
-    <div class="tab-pane fade show active" id={val[0].nid} role="tabpanel" aria-labelledby={val[0].title} tabindex="0">{val[0].body}</div>
+    <div class="tab-pane fade show active" id={val[0].nid} role="tabpanel" aria-labelledby={val[0].title} tabindex="0" >{Parser().parse(val[0].body)}</div>
    <div class="tab-pane fade" id={val[1].nid} role="tabpanel" aria-labelledby={val[1].title} tabindex="0">{`${val[1].assess_title}${val[1].assess_desc}${val[2].assess_title}${val[2].assess_desc}`}</div>
-    <div class="tab-pane fade" id={val[3].nid} role="tabpanel" aria-labelledby={val[3].title} tabindex="0">{val[3].body}</div>
+    <div class="tab-pane fade" id={val[3].nid} role="tabpanel" aria-labelledby={val[3].title} tabindex="0">{Parser().parse(val[3].body)}</div>
     <div class="tab-pane fade" id={val[4].nid} role="tabpanel" aria-labelledby={val[4].title} tabindex="0">
       {val[4].assess_title}<br/><img style={{width:"30%", height:"40%"}} src={val[4].assess_img} class="img-thumbnail" alt={val[4].assess_title}/><br/><br/>
       {val[5].assess_title}<br/><img style={{width:"30%", height:"40%"}} src={val[5].assess_img} class="img-thumbnail" alt={val[5].assess_title}/><br/><br/>
       {val[6].assess_title}<br/><img style={{width:"30%", height:"40%"}} src={val[6].assess_img} class="img-thumbnail" alt={val[5].assess_title}/><br/><br/>
       {val[7].assess_title}<br/><img style={{width:"30%", height:"40%"}} src={val[7].assess_img} class="img-thumbnail" alt={val[7].assess_title}/>
     </div>
-    <div class="tab-pane fade" id={val[8].nid} role="tabpanel" aria-labelledby={val[8].title} tabindex="0">{val[8].body}</div>
-    <div class="tab-pane fade" id={val[9].nid} role="tabpanel" aria-labelledby={val[9].title} tabindex="0">{val[9].body}</div>
-    <div class="tab-pane fade" id={val[10].nid} role="tabpanel" aria-labelledby={val[10].title} tabindex="0">{val[10].body}</div>
+    <div class="tab-pane fade" id={val[8].nid} role="tabpanel" aria-labelledby={val[8].title} tabindex="0">{Parser().parse(val[8].body)}</div>
+    <div class="tab-pane fade" id={val[9].nid} role="tabpanel" aria-labelledby={val[9].title} tabindex="0">{Parser().parse(val[9].body)}</div>
+    <div class="tab-pane fade" id={val[10].nid} role="tabpanel" aria-labelledby={val[10].title} tabindex="0">{Parser().parse(val[10].body)}</div>
   </div>
   
 </div>
